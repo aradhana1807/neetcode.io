a6dc0ffdbd1a32edd42e1d93649ceed5bddc8b90
Aradhana Nayak <aradhana.zero@gmail.com>
2024-11-21 19:04:53 +0530

    new codes

--- peakIndexMountainArray.cpp
+++ peakIndexMountainArray.cpp
@@ -9,10 +9,11 @@ public:
   {
     int start = 0;
     int end = arr.size() - 1;
-    int mid = start + (end - start) / 2;
 
     while (start < end)
     {
+      int mid = start + (end - start) / 2;
+      
       if (arr[mid] > arr[mid + 1])
       {
         end = mid;
